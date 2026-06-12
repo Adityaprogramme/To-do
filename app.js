dc8869b200563a692f44d2ce0da0d2f31a310895
Adityaprogramme <134223663+Adityaprogramme@users.noreply.github.com>
2026-06-12 08:26:26 +0530

    fixed

--- app.js
+++ app.js
@@ -9,10 +9,8 @@ const userRoutes = require("./routes/userRoute");
 app.use(express.json());
 app.use(
     cors({
-        origin: [
-            "http://localhost:5174",
-            "http://localhost:5173"
-        ]
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
 );
